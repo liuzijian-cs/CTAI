e7d7672f3e78babe18281e0957de593d75e931d5
刘子健_LiuZijian <alphatk@vip.qq.com>
2024-03-20 22:02:59 +0800

    update readme

--- CTAI_model/train.py
+++ CTAI_model/train.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 import torch
 import argparse
-from lib.base_function import print_log,draw_picture
+from lib.base_function import print_log, draw_picture
 from lib.data_function import get_dataloader
 from lib.dice_loss import dice
 from model.unet import UNet
@@ -15,7 +15,7 @@ parser.add_argument('--data_path', type=str, default='D:\CTAI_source\CTAI_model\
 parser.add_argument('--save', type=str, default='model_save', help='save path')  # 模型保存路径
 parser.add_argument('--log_file', type=str, default='model_save/log.txt', help='log file')  # 日志文件
 parser.add_argument('--test_ratio', type=float, default=0.2)  # 测试集比例(本项目未设置开发集)
-parser.add_argument('--seed', type=int, default=3407, help='random seed')  # 随机种子，如果设置为None 则完全随机
+parser.add_argument('--seed', type=int, default=None, help='random seed')  # 随机种子，如果设置为None 则完全随机
 parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate')  # 学习率
 parser.add_argument('--epochs', type=int, default=2)  # 训练轮数
 parser.add_argument('--threshold', type=float, default=0.5)  # 置信度
@@ -87,7 +87,7 @@ def main():
             loss.backward()
 
             # train dice:
-            y_pred_np = (y_pred.detach().cpu().numpy() > 0.5).astype(np.bool_)
+            y_pred_np = (y_pred.detach().cpu().numpy() > args.threshold).astype(np.bool_)
             batch_dice = dice(y.detach().cpu().numpy().astype(np.bool_), y_pred_np)
             epoch_train_dice += batch_dice
 
@@ -107,41 +107,30 @@ def main():
                 loss = criterion(y_pred, y)
                 epoch_test_loss += loss.item()
 
-                y_pred_np = (y_pred.detach().cpu().numpy() > 0.5).astype(np.bool_)
+                y_pred_np = (y_pred.detach().cpu().numpy() > args.threshold).astype(np.bool_)
                 batch_dice = dice(y.detach().cpu().numpy().astype(np.bool_), y_pred_np)
                 epoch_test_dice += batch_dice
-
-
-                # y_pred = model(x)
-                # loss = criterion(y,y_pred)
-                #
-                # y_np = y[1].detach().cpu().squeeze(0).numpy()
-                # y_pred_np = torch.squeeze(y_pred).detach().cpu().numpy()
-                # y_pred_np[y_pred_np >= args.threshold] = 1
-                # y_pred_np[y_pred_np < args.threshold] = 0
-                #
-                # epoch_test_loss += loss
-                # epoch_test_dice += dice(y_np, y_pred_np)
 
         t3_epoch = time.time()
 
         train_loss_ = epoch_train_loss / len(train_loader)
         test_loss_ = epoch_test_loss / len(test_loader)
         train_dice_ = epoch_train_dice / len(train_loader)
-        test_dice_ = epoch_test_dice/len(test_loader)
+        test_dice_ = epoch_test_dice / len(test_loader)
 
         train_loss.append(train_loss_)
         test_loss.append(test_loss_)
         train_dice.append(train_dice_)
         test_dice.append(test_dice_)
 
-        print_log(args, f"epoch: {epoch:03d}, train loss:{train_loss_:.5f}, test loss: {test_loss_:.5f}, train dice: {train_dice_:.5f}, test dice: {test_dice_:.5f}, epoch time cost: {(t3_epoch-t1_epoch):.2f} (train: {(t2_epoch - t1_epoch):.2f}, test: {(t3_epoch-t2_epoch):.2f})")
+        print_log(args,
+                  f"epoch: {epoch:03d}, train loss:{train_loss_:.5f}, test loss: {test_loss_:.5f}, train dice: {train_dice_:.5f}, test dice: {test_dice_:.5f}, epoch time cost: {(t3_epoch - t1_epoch):.2f} (train: {(t2_epoch - t1_epoch):.2f}, test: {(t3_epoch - t2_epoch):.2f})")
 
-
-        torch.save(model.state_dict(),os.path.join(args.save, f"{epoch:03d}.pth")) # 模型保存
+        torch.save(model.state_dict(), os.path.join(args.save, f"{epoch:03d}.pth"))  # 模型保存
     # draw
     draw_picture(args, train_loss, test_loss, train_dice, test_dice)
     log.close()
+
 
 if __name__ == "__main__":
     time_start = time.time()
