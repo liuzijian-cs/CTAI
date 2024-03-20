13f50a37501b73cac9a243444ff6ed513596435e
刘子健_LiuZijian <alphatk@vip.qq.com>
2024-03-20 20:59:54 +0800

    update 240320

--- CTAI_model/lib/base_function.py
+++ CTAI_model/lib/base_function.py
@@ -13,14 +13,9 @@ def print_log(args, string):
     log.flush()
     print(string)
 
-def dice_coefficient(y_true, y_pred):
-    smooth = 1e-6 # 避免除以零
-    y_true_flatten = np.asarray(y_true).flatten()
-    y_pred_flatten = np.asarray(y_pred).flatten()
-    intersection = np.sum(y_true_flatten * y_pred_flatten)
-    coefficient = (2. * intersection + smooth) / (np.sum(y_true_flatten)+np.sum(y_pred_flatten)+smooth)
-    return coefficient
 
+
+# 绘图并输出
 def draw_picture(args,train_loss,train_dice,test_loss,test_dice):
     plt.figure(figsize=(16,12))
     epochs = range(args.epochs)
@@ -58,4 +53,32 @@ def draw_picture(args,train_loss,train_dice,test_loss,test_dice):
     plt.legend()
     plt.savefig(os.path.join(args.save, "test_dice.png"))
 
+    plt.figure(figsize=(16,12))
+    epochs = range(args.epochs)
+    plt.plot(epochs,train_loss,"b",label='Train Loss')
+    plt.plot(epochs,test_loss,"r",label='Test Loss')
+    plt.title('Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(os.path.join(args.save, "loss.png"))
 
+    plt.figure(figsize=(16,12))
+    epochs = range(args.epochs)
+    plt.plot(epochs,train_dice,"b",label='Train Dice')
+    plt.plot(epochs,test_dice,"r",label='Test Dice')
+    plt.title('Dice')
+    plt.xlabel('Epoch')
+    plt.ylabel('Dice')
+    plt.legend()
+    plt.savefig(os.path.join(args.save, "dice.png"))
+
+
+# 弃用
+# def dice_coefficient(y_true, y_pred):
+#     smooth = 1e-6 # 避免除以零
+#     y_true_flatten = np.asarray(y_true).flatten()
+#     y_pred_flatten = np.asarray(y_pred).flatten()
+#     intersection = np.sum(y_true_flatten * y_pred_flatten)
+#     coefficient = (2. * intersection + smooth) / (np.sum(y_true_flatten)+np.sum(y_pred_flatten)+smooth)
+#     return coefficient
